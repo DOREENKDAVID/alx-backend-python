83457d4f70f3cefa3a385471fb78c30d4f8417dc
DOREENKDAVID <doreenkinya153@gmail.com>
2023-12-07 14:46:52 -0800

    Advanced

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""add type annotations to the function TypeVar"""
+from typing import Mapping, TypeVar, Any, Union
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None)\
+        -> Union[Any, T]:
+    """TypeVar"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
